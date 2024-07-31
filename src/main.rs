3e3f99444f6ec13aca6aebc92a790d9539b31d6b
Clifford T. Matthews <ctm@devctm.com>
2024-07-31 09:03:26 -0600

    shows that if we create our own ErrorEvent that we can get its message

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use yew::prelude::*;
 
+use {
+    web_sys::{
+        wasm_bindgen::{closure::Closure, JsCast},
+        ErrorEvent, ErrorEventInit, WebSocket
+    },
+};
+
+
 #[function_component(App)]
 fn app() -> Html {
+    let message = match ErrorEvent::new("error") {
+        Err(e) => format!("Failed to create an ErrorEvent: {e:?}"),
+        Ok(ee) => format!("message: {}", ee.message()),
+    };
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "Whee" }</h1>
+            <p>{ message }</p>
+        </>
     }
 }
 
